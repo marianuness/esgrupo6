9d6121c86dcc870424a542ad33b079edd8fd4f33
matheusturatti <matheusturatti@outlook.com>
2019-05-20 02:06:24 -0300

    Funções relacionadas a Cadastrar/Editar produtos

--- Códigos/produto.php
+++ Códigos/produto.php
@@ -10,6 +10,15 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="publico/css/estilo.css">
+	<style>
+	table, th, td {
+	border: 1px solid black;
+	border-collapse: collapse;
+	}
+	th, td {
+	padding: 15px;
+	}
+	</style>
 </head>
 
 <?php
@@ -21,15 +30,49 @@
 		if(isset($_SESSION['logado']) && $_SESSION['logado'] && $_SESSION['tipo_usuario'] == 'Funcionario'){
 			?>
 
-			<a href="#"> Cadastrar Produto </a> </br>
-			<a href="#"> Editar Produto </a> </br>
-			<a href="#"> Excluir Produto </a>
+			<a href="produto_cadastro.php"> Cadastrar Produto </a> </br>
 			<p> </p>
 
 			<?php
 		}
+		
 	?>
 
+<br>
+<table>
+  <tr>
+    <th>Nome</th>
+    <th>Preço</th> 
+    <th>Descrição</th>
+	<?php
+	if(isset($_SESSION['logado']) && $_SESSION['logado'] && $_SESSION['tipo_usuario'] == 'Funcionario'){
+	echo '<th>Editar</th>';
+	echo '<th>Excluir</th>';
+	}
+	?>
+  </tr>
+ <?php
+	include_once("conexao.php");
+ 	$sql = "SELECT * FROM produto";
+	$produtos = mysqli_query($conexao, $sql);
+	
+	foreach($produtos as $produto){
+		$sql = "SELECT * FROM produto WHERE id='".$produto['id']."'";
+		$selecionado = mysqli_fetch_array( mysqli_query($conexao, $sql) );
+		
+		echo "<tr>";
+		echo '<td>' . $selecionado['nome'] . '</td>';
+		echo '<td>' . $selecionado['preco'] . '</td>';
+		echo '<td>' . $selecionado['descricao'] . '</td>';
+		if(isset($_SESSION['logado']) && $_SESSION['logado'] && $_SESSION['tipo_usuario'] == 'Funcionario'){
+			echo '<td> <a href="produto_editar.php?id='.$selecionado['id'].'"> Editar Produto </a> </td>';
+			echo '<td> <a href="produto_excluir.php?id='.$selecionado['id'].'"> Excluir Produto </a> </td>';
+		}
+		echo "</tr>";
+	}
+?>
+</table>
+<br>
 </body>
 
 <?php
---
+++ Códigos/produto_editar.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Edição de Produto</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="publico/css/bootstrap.min.css" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="js/utils.js"></script>
+	<link rel="stylesheet" href="publico/css/estilo.css">
+</head>
+
+<?php
+	include "header.php";
+?>
+
+<hr>
+
+<body>
+	<!-- Script para fazer a máscara. Com ele, você pode definir qualquer tipo de máscara com o comando onkeypress="mascara(this, '###.###.###-##')". -->
+	<script language="JavaScript">
+		function mascara(t, mask){
+			var i = t.value.length;				
+			var saida = mask.substring(1,0);
+			var texto = mask.substring(i)
+			if (texto.substring(0,1) != saida){
+				t.value += texto.substring(0,1);
+			}
+		}
+	</script>
+	<!-- Fim do script -->
+	<!-- Formulário de Cadastro de Usuário -->
+	<?php
+		$id = $_GET['id'];
+		include_once("conexao.php");	/* Estabelece a conexão */
+
+		$sql = "SELECT * FROM produto WHERE id=" . $id;
+		$produto = mysqli_fetch_array( mysqli_query($conexao, $sql) );
+	?>
+	
+	<form action="" method="POST" target="_self">
+		<div class="form-group col-md-4">
+	<fieldset>
+		<legend>Informações Pessoais:</legend>
+		<div class="form-row">
+			<div class="form-group col-md-6">
+			<label for="inputNome4">Nome</label>
+			<input type="name" name="nome" class="form-control" id="inputNome4" placeholder="Nome" value="<?php  echo $produto['nome']; ?>">
+			</div>
+			<div class="form-group col-md-6">
+			<label for="inputPrice4">Preço</label>
+			<input type="text" name="preco" class="form-control" id="inputPrice4" placeholder="Preco" value="<?php  echo $produto['preco']; ?>">
+			</div>
+			<div class="form-group col-md-6">
+			<label for="inputDescription4">Descrição</label>
+			<input type="text" name="descricao" class="form-control" id="inputDescription4" placeholder="Descrição" value="<?php  echo $produto['descricao']; ?>">
+			</div>
+		</div>
+	</fieldset>
+		</div>
+	</fieldset>
+	</br>
+	</br>
+	<button type="submit" class="btn btn-primary" value="Submit" name="submit">Confirmar</button>
+	</form>
+	<!-- Fim do Formulário de Cadastro de Usuário	-->
+	
+	<?php
+		/* Ligação com Banco de Dados */
+		if(isset($_POST["submit"])) {
+			include_once("conexao.php");	/* Estabelece a conexão */
+
+			$nome = $_POST['nome'];
+			$preco = $_POST['preco'];
+			$descricao = $_POST['descricao'];
+
+			$sql = "UPDATE produto SET nome = '".$nome."', preco = '".$preco."', descricao ='". $descricao."' WHERE id = ".$produto['id']."";
+			$resultado = mysqli_query($conexao, $sql);
+
+			if($resultado){
+				?>
+				<div class="alert alert-success">Produto atualizado com sucesso!</div>
+				<?php
+			}
+			else{
+				die(mysqli_error($conexao));
+				?>
+				<div class="alert alert-warning">Falha ao atualizar produto!</div>
+				<?php
+			}
+		
+			mysqli_close($conexao);
+		}
+	?>
+
+</body>
+
+<hr>
+
+<?php
+	include "footer.php";
+?>
+
+</html>
